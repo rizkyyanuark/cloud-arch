b84a3120fb1345e1bfef9311e56a0f1b120fa052
Rizky Yanuar Kristianto <rizkyyanuarkristianto@gmail.com>
2026-09-08 09:11:31 +0700

    fix(deploy): add ubuntu user to docker group

--- deploy.sh
+++ deploy.sh
@@ -53,6 +53,7 @@ if ! command -v docker &> /dev/null; then
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt-get update
   apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  usermod -aG docker ubuntu || true
   systemctl enable docker
   systemctl start docker
   echo "Docker Engine berhasil diinstall."
